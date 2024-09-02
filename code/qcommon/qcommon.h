50f980aea44e4f11eb8226e352c3814071ba7aa9
Michel Courtine <michak@michak.net>
2024-09-02 18:18:15 +0200

    :wrench: (qcommon): pointed master server to quakejs

--- code/qcommon/qcommon.h
+++ code/qcommon/qcommon.h
@@ -264,7 +264,7 @@ extern int demo_protocols[];
 #endif
 // override on command line, config files etc.
 #ifndef MASTER_SERVER_NAME
-#define MASTER_SERVER_NAME	"master.quake3arena.com"
+#define MASTER_SERVER_NAME	"master.quakejs.com"
 #endif
 
 #ifndef STANDALONE
